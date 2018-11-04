d2b5f27c661574e8d5270dccc808a3cd02370406
juha62 <juha.m.pusa@hotmail.com>
2018-11-04 15:52:20 +0200

    Muutoksia databaseen. Logi ja rekisteröinti ok. Datbaseen ei saa yhteyttä

--- AsTiKa/Controllers/HomeController.cs
+++ AsTiKa/Controllers/HomeController.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace AsTiKa.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+            return this.View();
         }
 
         public ActionResult About()
